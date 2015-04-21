a23da4bd2da096fea7465f15facb8f3800bf9d37
Michael Johnson <chaplin13@gmail.com>
2015-04-21 18:24:47 -0500

    Added Score and SpriteKit scene

--- AAOSteroids/GameScene.swift
+++ AAOSteroids/GameScene.swift
@@ -44,6 +44,7 @@ extension CGPoint {
     }
 }
 
+//Music
 func playBackgroundMusic(filename: String) {
     let url = NSBundle.mainBundle().URLForResource(
         filename, withExtension: nil)
@@ -65,39 +66,44 @@ func playBackgroundMusic(filename: String) {
     backgroundMusicPlayer.play()
 }
 
+var score:Int = 0 //The variable holding the score.
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    // 1
     let player = SKSpriteNode(imageNamed: "ship")
 
     
+    func updateScoreWithValue (value: Int) {
+        var lblScore = self.childNodeWithName("scoreDisplayLabel") as! SKLabelNode;
+        score += value
+        lblScore.text = "SCORE: \(score)"
+    }
+    
     override func didMoveToView(view: SKView) {
         playBackgroundMusic("background-music-aac.caf")
+
+        //set up Physics
         physicsWorld.gravity = CGVectorMake(0, 0)
         physicsWorld.contactDelegate = self
         
-        // 2
         backgroundColor = SKColor.blackColor()
-        // 3
         player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
-        // 4
         
+        //Set up the collision detection
         player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width/2) // 1
         player.physicsBody?.dynamic = false // 2
         player.physicsBody?.categoryBitMask = PhysicsCategory.Player // 3
-        player.physicsBody?.contactTestBitMask = PhysicsCategory.Monster // 4
+        player.physicsBody?.contactTestBitMask = PhysicsCategory.Asteroid // 4
         player.physicsBody?.collisionBitMask = PhysicsCategory.None // 5
         
         addChild(player)
         
-
+        //Here you run a sequence of actions to call a block of code (you can seamlessly pass in your addAsteroid() method here thanks to the power of Swift), and then wait for 1 second. You then repeat this sequence of actions endlessly.
         
-        //Here you run a sequence of actions to call a block of code (you can seamlessly pass in your addMonster() method here thanks to the power of Swift), and then wait for 1 second. You then repeat this sequence of actions endlessly.
         runAction(SKAction.repeatActionForever(
             SKAction.sequence([
-                SKAction.runBlock(addMonster),
-                SKAction.waitForDuration(1.0)
+                SKAction.runBlock(addAsteroid),
+                SKAction.waitForDuration(0.8)
                 ])
             ))
     }
@@ -105,8 +111,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     struct PhysicsCategory {
         static let None      : UInt32 = 0
         static let All       : UInt32 = UInt32.max
-        static let Monster   : UInt32 = 0b1       // 1
-        static let Projectile: UInt32 = 0b10      // 2
+        static let Asteroid   : UInt32 = 0b1       // 1
+        static let EnergyBall: UInt32 = 0b10      // 2
         static let Player    : UInt32 = 0b100
     }
     
@@ -118,66 +124,66 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         return random() * (max - min) + min
     }
     
-    func addMonster() {
+    func addAsteroid() {
         
         // Create sprite
-        let monster = SKSpriteNode(imageNamed: "rocks")
+        let asteroid = SKSpriteNode(imageNamed: "rocks")
         
-        monster.physicsBody = SKPhysicsBody(circleOfRadius: monster.size.width/2) // 1
-        monster.physicsBody?.dynamic = true // 2
-        monster.physicsBody?.categoryBitMask = PhysicsCategory.Monster // 3
-        monster.physicsBody?.contactTestBitMask = PhysicsCategory.Projectile // 4
-        monster.physicsBody?.collisionBitMask = PhysicsCategory.None // 5
-        monster.physicsBody?.friction = 0.8
+        asteroid.physicsBody = SKPhysicsBody(circleOfRadius: asteroid.size.width/2) // 1
+        asteroid.physicsBody?.dynamic = true // 2
+        asteroid.physicsBody?.categoryBitMask = PhysicsCategory.Asteroid // 3
+        asteroid.physicsBody?.contactTestBitMask = PhysicsCategory.EnergyBall // 4
+        asteroid.physicsBody?.collisionBitMask = PhysicsCategory.None // 5
+        asteroid.physicsBody?.friction = 0.5
         
-        // Determine where to spawn the monster along the Y axis
-        let actualY = random(min: monster.size.height/2, max: size.height - monster.size.height/2)
+        // Determine where to spawn the asteroid along the Y axis
+        let actualY = random(min: asteroid.size.height/2, max: size.height - asteroid.size.height/2)
         
-        // Position the monster slightly off-screen along the right edge,
+        // Position the asteroid slightly off-screen along the right edge,
         // and along a random position along the Y axis as calculated above
-        monster.position = CGPoint(x: size.width + monster.size.width/2, y: actualY)
+        asteroid.position = CGPoint(x: size.width + asteroid.size.width/2, y: actualY)
         
-        // Add the monster to the scene
-        addChild(monster)
+        // Add the asteroid to the scene
+        addChild(asteroid)
         
-        // Determine speed of the monster
+        // Determine speed of the asteroid
         let actualDuration = random(min: CGFloat(2.0), max: CGFloat(4.0))
         
         // Create the actions
-        //let actionMove = SKAction.moveTo(CGPoint(x: -monster.size.width/2, y: actualY), duration: NSTimeInterval(actualDuration))
+        //let actionMove = SKAction.moveTo(CGPoint(x: -asteroid.size.width/2, y: actualY), duration: NSTimeInterval(actualDuration))
         
         
         let actionMoveDone = SKAction.removeFromParent()
-        monster.physicsBody!.applyImpulse(CGVectorMake(-actualDuration*10, 0))
+        asteroid.physicsBody!.applyImpulse(CGVectorMake(-actualDuration*10, 0))
         
     }
     
     override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
         
         runAction(SKAction.playSoundFileNamed("pew-pew-lei.caf", waitForCompletion: true))
-        // 1 - Choose one of the touches to work with
+        // Choose one of the touches to work with
         let touch = touches.first as! UITouch
         let touchLocation = touch.locationInNode(self)
         
-        // 2 - Set up initial location of projectile
-        let projectile = SKSpriteNode(imageNamed: "energyBall")
-        projectile.position = player.position
+        // Set up initial location of energyBall
+        let energyBall = SKSpriteNode(imageNamed: "energyBall")
+        energyBall.position = player.position
         
-        projectile.physicsBody = SKPhysicsBody(circleOfRadius: projectile.size.width/2)
-        projectile.physicsBody?.dynamic = false
-        projectile.physicsBody?.categoryBitMask = PhysicsCategory.Projectile
-        projectile.physicsBody?.contactTestBitMask = PhysicsCategory.Monster
-        projectile.physicsBody?.collisionBitMask = PhysicsCategory.None
-        projectile.physicsBody?.usesPreciseCollisionDetection = true
+        energyBall.physicsBody = SKPhysicsBody(circleOfRadius: energyBall.size.width/2)
+        energyBall.physicsBody?.dynamic = false
+        energyBall.physicsBody?.categoryBitMask = PhysicsCategory.EnergyBall
+        energyBall.physicsBody?.contactTestBitMask = PhysicsCategory.Asteroid
+        energyBall.physicsBody?.collisionBitMask = PhysicsCategory.None
+        energyBall.physicsBody?.usesPreciseCollisionDetection = true
         
-        // 3 - Determine offset of location to projectile
-        let offset = touchLocation - projectile.position
+        // 3 - Determine offset of location to energyBall
+        let offset = touchLocation - energyBall.position
         
         // 4 - Bail out if you are shooting down or backwards
         if (offset.x < 0) { return }
         
         // 5 - OK to add now - you've double checked position
-        addChild(projectile)
+        addChild(energyBall)
         
         // 6 - Get the direction of where to shoot
         let direction = offset.normalized()
@@ -186,22 +192,23 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let shootAmount = direction * 1000
         
         // 8 - Add the shoot amount to the current position
-        let realDest = shootAmount + projectile.position
+        let realDest = shootAmount + energyBall.position
         
         // 9 - Create the actions
         let actionMove = SKAction.moveTo(realDest, duration: 2.0)
         let actionMoveDone = SKAction.removeFromParent()
-        projectile.runAction(SKAction.sequence([actionMove, actionMoveDone]))
+        energyBall.runAction(SKAction.sequence([actionMove, actionMoveDone]))
         
     }
     
-    func projectileDidCollideWithMonster(projectile:SKSpriteNode, monster:SKSpriteNode) {
+    func energyBallDidCollideWithAsteroid(energyBall:SKSpriteNode, asteroid:SKSpriteNode) {
         println("Hit")
-        projectile.removeFromParent()
-        monster.removeFromParent()
+        updateScoreWithValue(1);
+        energyBall.removeFromParent()
+        asteroid.removeFromParent()
     }
     
-    func projectileDidCollideWithPlayer(projectile:SKSpriteNode, player:SKSpriteNode){
+    func energyBallDidCollideWithPlayer(energyBall:SKSpriteNode, player:SKSpriteNode){
         println("dead!")
         player.removeFromParent()
         
@@ -221,14 +228,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
         
         // 2
-        if ((firstBody.categoryBitMask & PhysicsCategory.Monster != 0) &&
-            (secondBody.categoryBitMask & PhysicsCategory.Projectile != 0)) {
-                projectileDidCollideWithMonster(firstBody.node as! SKSpriteNode, monster: secondBody.node as! SKSpriteNode)
+        if ((firstBody.categoryBitMask & PhysicsCategory.Asteroid != 0) &&
+            (secondBody.categoryBitMask & PhysicsCategory.EnergyBall != 0)) {
+                energyBallDidCollideWithAsteroid(firstBody.node as! SKSpriteNode, asteroid: secondBody.node as! SKSpriteNode)
         }
         
-        if((firstBody.categoryBitMask & PhysicsCategory.Monster != 0) &&
+        if((firstBody.categoryBitMask & PhysicsCategory.Asteroid != 0) &&
             (secondBody.categoryBitMask & PhysicsCategory.Player != 0)) {
-                projectileDidCollideWithPlayer(firstBody.node as! SKSpriteNode, player: secondBody.node as! SKSpriteNode)
+                energyBallDidCollideWithPlayer(firstBody.node as! SKSpriteNode, player: secondBody.node as! SKSpriteNode)
         }
         
         
