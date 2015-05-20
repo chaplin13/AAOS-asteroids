d37837b68f77d40a4cac343c5727020ff8d8ed15
Michael Johnson <chaplin13@gmail.com>
2015-05-20 13:23:33 -0500

    Continue Button

--- AAOSteroids/GameOverScene.swift
+++ AAOSteroids/GameOverScene.swift
@@ -28,9 +28,14 @@ class GameOverScene: SKScene {
         label.position = CGPoint(x: size.width/2, y: size.height/2)
         addChild(label)
         
-        // 4
+        var button: SKButton = SKButton(defaultButtonImage: "continueButton", activeButtonImage: "continueButton_active", buttonAction: goToGame)
+        button.position = CGPointMake(label.position.x, label.position.y - label.frame.height * 1.5)
+        addChild(button)
+        
+    }
+
+    func goToGame() {
         runAction(SKAction.sequence([
-            SKAction.waitForDuration(3.0),
             SKAction.runBlock() {
                 // 5
                 let reveal = SKTransition.flipHorizontalWithDuration(0.5)
@@ -39,11 +44,11 @@ class GameOverScene: SKScene {
                 }
             }
             ]))
-        
     }
+
     
-    // 6
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
 }
---
+++ AAOSteroids/GameStartScene.swift
@@ -0,0 +1,14 @@
+//
+//  GameStartScene.swift
+//  AAOSteroids
+//
+//  Created by Michael Johnson on 5/17/15.
+//  Copyright (c) 2015 BobbingHeads. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameStartScene: SKScene {
+   
+}
